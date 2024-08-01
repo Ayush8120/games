988f63e2aa19dc9f9b033464849af8c1aa45878e
Ayush Agrawal <ay.agrawal812@gmail.com>
2024-08-01 17:23:40 +0530

    threading-bug-fix

--- hangman.py
+++ hangman.py
@@ -1,5 +1,5 @@
 from hangman import utils
-import threading #useful for shared_flag between process - tried but removed
+import threading 
 import re, os, time
 
 '''
@@ -7,43 +7,53 @@ countdown creation
 '''
 def countdown(lock, t): 
     global flag
-    
-    print('Try to do 3 within 2 minutes time')
-    while t: 
+    while t and not flag: 
         mins, secs = divmod(t, 60) 
         timer = '\n\n---------------------- Time : {:02d}:{:02d} ---------------------- \n\n'.format(mins, secs) 
         print(timer, end="\r") 
+        if flag:
+            break
         time.sleep(5) 
         t -= 5
-    lock.acquire()
-    flag = True
-    lock.release()
+    if not flag:
+        lock.acquire()
+        flag = True
+        lock.release()
 
 def setup():
-    movieList = utils.give_movie(3)
+    while True:
+        try:
+            rounds = int(input('How many words can you do in 2 minutes?\n'))
+            break
+        except:
+            continue
+    movieList = utils.give_movie(rounds)
     maskList = utils.mask_words(movieList)
-    return movieList, maskList
+    return rounds, movieList, maskList
 
-def play(movieList:list, maskList:list):
+def play(rounds:int, movieList:list, maskList:list):
     score = 0
     global flag
     for i,movie in enumerate(movieList):
+        guess_hist = []
         wrong_count = 0
-        print(f'WORD : {maskList[i]}\n\n')
+        print(f'WORD : {maskList[i]}\n')
         while True:
            try:
             assert flag == False 
             guess = input('Enter your Guess : ')
             assert ord(guess.lower()) >=97 and ord(guess.lower()) <= 122 
+            guess_hist.append(guess)
             if '_' in maskList[i]:
                 if guess.lower() in movie:
                     temp = list(maskList[i].replace(" ",""))
                     indexes = [x.start() for x in re.finditer(guess.lower(), movie)]
                     space_idx = [x.start() for x in re.finditer(' ', movie)]
                     for p in space_idx : temp.insert(p, " ")
-                    for z in indexes: temp[z] = guess
-                    print('WORD : '+" ".join(temp) +'\n\n')
+                    for z in indexes: temp[z] = guess.lower()
+                    print('WORD : '+" ".join(temp) +'\n')
                     maskList[i] = " ".join(temp)
+                    print('Great Guess!\n')
                     if '_' in maskList[i]:
                         continue
                     else:
@@ -53,8 +63,10 @@ def play(movieList:list, maskList:list):
                     
                 elif guess.lower() not in movie:
                     wrong_count += 1
+                    print('Wrong Guess!')
                     if wrong_count <6:
                         utils.draw_hangman(wrong_count-1)
+                        print(f'WORD : {maskList[i]}' +'\n')
                         continue
                     else:
                         utils.draw_hangman(wrong_count)
@@ -71,20 +83,25 @@ def play(movieList:list, maskList:list):
                continue
         if flag:
             break
-    if not flag and score/3 == 0:
+        
+    if not flag and score < rounds:
         print('Out of attempts')
         print(f'Your Score : {score}')
-    elif not flag and score/3 != 0:
+    elif not flag and score == rounds:
+        print('Congrats!!')
         print(f'Your Score : {score}')
+    lock.acquire()
+    flag = True
+    lock.release()
     
 
 if __name__ == "__main__":
-    movieList, maskList = setup()
+    rounds, movieList, maskList = setup()
     lock = threading.Lock()
     flag = False
-    countdown = threading.Thread(target = countdown, args=(lock,10,))
+    countdown = threading.Thread(target = countdown, args=(lock,120,))
     countdown.start()
-    gameplay = threading.Thread(target=play, args=(movieList, maskList,))
+    gameplay = threading.Thread(target=play, args=(rounds, movieList, maskList,))
     gameplay.start()
     
 
