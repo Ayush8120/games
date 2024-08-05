3a3cde815203d7b8c95fb23c202731f39cea320d
Ayush Agrawal <72944387+Ayush8120@users.noreply.github.com>
2024-08-05 23:33:18 +0530

    game layout

--- house.py
+++ house.py
@@ -1,3 +1,13 @@
 '''
 a dungeon type game for exploration , interaction and task completion
+- single player
+- rooms 
+- objects
+- object explore option
+- object physical state as a description/video
+- tasks
+- actions [pick,place,open,clean,...]
+- rewards
+- time 
+- gameplay recording option
 '''
